c0d8203ef855caff95a7b236a2be5e3ded1af60d
Shalabh Agarwal <sagarwal10@hotmail.com>
2015-10-25 12:18:29 -0700

    Added s3 host name

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,7 +51,8 @@ Rails.application.configure do
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+    :s3_host_name => 'animalwishes.s3.amazonaws.com'
   }
  }
 end
