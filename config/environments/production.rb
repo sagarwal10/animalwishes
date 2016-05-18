0a37849d9f584acf2010bffb12caf641c581dc71
Shalabh <sagarwal10@hotmail.com>
2016-05-18 18:18:32 +0530

    Add s3 region to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,11 +90,11 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
     :storage => :s3,
+    :s3_region => ENV['AWS_S3_REGION'],
     :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :s3_region => ENV['AWS_S3_REGION']
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
 
 #  config.to_prepare { Devise::SessionsController.force_ssl }
