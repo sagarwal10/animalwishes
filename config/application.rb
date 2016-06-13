ac6c5c1373a2a3e92e7a649a7a3d1047444896ea
Shalabh Agarwal <sagarwal10@hotmail.com>
2016-06-13 14:48:14 -0700

    Go back to trying force ssl and full certificate on cloudflare

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,7 @@ module Animalwishes
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    # config.force_ssl = true
+    config.force_ssl = true
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
